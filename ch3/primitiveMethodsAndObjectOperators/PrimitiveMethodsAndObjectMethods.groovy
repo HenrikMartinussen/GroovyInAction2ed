a40fc309714e846f9aebc3d7ccf2ec68d235a825
Henrik Martinussen <hma@mail.dk>
2014-01-23 23:29:41 +0100

    some chapter 3 examples

---
+++ ch3/primitiveMethodsAndObjectOperators/PrimitiveMethodsAndObjectMethods.groovy
@@ -0,0 +1,14 @@
+//ReGinA p 55
+println (60 * 60 * 24 *365).toString();//invalid java
+
+int secondsPerYear = 60 * 60 * 24 * 365;
+secondsPerYear.toString(); // invalid java
+
+new Integer(secondsPerYear).toString();
+
+assert "abc" - "a" == "bc"
+assert "abc" - "b" == "ac"
+assert "abc" - "c" == "ab"
+
+
+println 60*60
