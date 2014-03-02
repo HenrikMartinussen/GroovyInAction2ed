0d7982aabe30244bacf821c8f2e9ec850885fb9e
henmar <hma@mail.dk>
2014-03-02 22:46:07 +0100

    added example of groovy scripting the parsing of a line read from a text file, returning the result in a Map

---
+++ other/java-integration/mrhaki-example/readme.txt
@@ -0,0 +1,7 @@
+http://mrhaki.blogspot.dk/2009/11/groovy-goodness-running-groovy-scripts.html
+http://stackoverflow.com/questions/5868369/how-to-read-a-large-text-file-line-by-line-using-java
+
+depends on groovy-all.jar being on the classpath
+
+
+gradle clean compileJava runSimple
---
+++ other/java-integration/mrhaki-example/src/main/java/info/martinussen/regina/integration/mrhakiex/Receiver.java
@@ -0,0 +1,56 @@
+package info.martinussen.regina.integration.mrhakiex;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+import org.codehaus.groovy.control.CompilationFailedException;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+
+public class Receiver{
+
+	public static void main (String[] args) throws CompilationFailedException, IOException, InstantiationException, IllegalAccessException{
+		final GroovyClassLoader classLoader = new GroovyClassLoader();
+		Class groovyClass = classLoader.parseClass(new File("src/main/resources/SampleScript.groovy"));
+		GroovyObject groovyObject = (GroovyObject) groovyClass.newInstance();
+		
+		
+		
+		BufferedReader br = new BufferedReader(new FileReader(new File("src/test/resources/transferorder.csv")));
+		String line;
+		int lineCounter = 0;
+		while ((line = br.readLine()) != null) {
+			Map<String, String> output = (Map<String, String>) groovyObject.invokeMethod("parseLine", new Object[] {new Integer(lineCounter), line});
+			lineCounter++;
+			if (output.size() > 0){
+				printOrderLine(output);
+			}
+		}
+		br.close();
+		classLoader.close();
+	}
+	
+	private static void printOrderLine(Map orderLine){
+		StringBuilder builder = new StringBuilder();
+		builder.append("OrderLine[");
+		builder.append("OrderNumber:");
+		builder.append(orderLine.get("orderNumber"));
+		builder.append(", ");
+		builder.append("LineNumber:");
+		builder.append(orderLine.get("lineNumber"));
+		builder.append(", ");
+		builder.append("ArticleNumber:");
+		builder.append(orderLine.get("articleNumber"));
+		builder.append(", ");
+		builder.append("Quantity:");
+		builder.append(orderLine.get("quantity"));
+		builder.append("]");
+		System.out.println(builder.toString());
+		
+	}
+
+}
