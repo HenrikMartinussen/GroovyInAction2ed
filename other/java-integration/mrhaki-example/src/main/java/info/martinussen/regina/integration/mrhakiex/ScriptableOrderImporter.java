3153170f57994b99dcdfd63b26e88506e24e58fd
henmar <henrik.martinussen@gmail.com>
2014-03-06 23:01:56 +0100

    changed the order print method slightly

--- other/java-integration/mrhaki-example/src/main/java/info/martinussen/regina/integration/mrhakiex/ScriptableOrderImporter.java
+++ other/java-integration/mrhaki-example/src/main/java/info/martinussen/regina/integration/mrhakiex/ScriptableOrderImporter.java
@@ -5,6 +5,8 @@ import groovy.lang.GroovyObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import org.codehaus.groovy.control.CompilationFailedException;
 
@@ -14,32 +16,35 @@ public class ScriptableOrderImporter{
 		final GroovyClassLoader classLoader = new GroovyClassLoader();
 		Class groovyClass = classLoader.parseClass(new File("src/main/resources/ImportScript.groovy"));
 		GroovyObject groovyObject = (GroovyObject) groovyClass.newInstance();
-		
-		
-		
+
+
+
 		File orderFile = new File("src/test/resources/transferorder.csv");
 		OrderBuilder builder = (OrderBuilder) groovyObject.invokeMethod("importFile", new Object[] {new OrderBuilder(), orderFile});
 		printOrder(builder);
-//		classLoader.close();
+		//		classLoader.close();
 	}
-	
+
 	private static void printOrder(OrderBuilder orderBuilder){
+		System.out.println("Ordernumber:" + orderBuilder.getOrderNumber());
+		List<Map<String,String>> orderlines = orderBuilder.getOrderLines();
 		StringBuilder builder = new StringBuilder();
-		builder.append("OrderLine[");
-		builder.append("OrderNumber:");
-		builder.append(orderBuilder.getOrderNumber());
-		builder.append(", ");
-		builder.append("LineNumber:");
-		builder.append(orderBuilder.getOrderLines().get(0).get("lineNumber"));
-		builder.append(", ");
-		builder.append("ArticleNumber:");
-		builder.append(orderBuilder.getOrderLines().get(0).get("articleNumber"));
-		builder.append(", ");
-		builder.append("Quantity:");
-		builder.append(orderBuilder.getOrderLines().get(0).get("quantity"));
-		builder.append("]");
-		System.out.println(builder.toString());
-		
+		for (int i = 0; i < orderlines.size(); i++){
+
+			builder.append("OrderLine[" + i + "][");
+			builder.append("LineNumber:");
+			builder.append(orderlines.get(i).get("lineNumber"));
+			builder.append(", ");
+			builder.append("ArticleNumber:");
+			builder.append(orderlines.get(i).get("articleNumber"));
+			builder.append(", ");
+			builder.append("Quantity:");
+			builder.append(orderlines.get(i).get("quantity"));
+			builder.append("]");
+			System.out.println(builder.toString());
+			builder.setLength(0);
+		}
+
 	}
 
 }
