56e6c9329764020db0df1b5d312e2eda06e53821
henmar <henrik.martinussen@gmail.com>
2014-03-07 00:48:06 +0100

    made the import a "real" Groovy Script by omitting the class wrapper

--- other/java-integration/mrhaki-example/src/main/resources/ImportScript.groovy
+++ other/java-integration/mrhaki-example/src/main/resources/ImportScript.groovy
@@ -1,25 +1,23 @@
 import info.martinussen.regina.integration.mrhakiex.OrderBuilder
 
-class ImportScript {
-	OrderBuilder importFile(OrderBuilder builder, File file){
-		builder.clear()
-		def lineCounter = 0
-		file.eachLine { 
-			if (lineCounter != 0) {
-				def values =  it.split(';')
-				assert values.size() == 4
-				if (!builder.isInitialized()){
-					builder.setOrderNumber(values[0])
-					builder.init()
-				}
-				builder.setLineNumber(values[1].toInteger())
-				builder.setArticleNumber(values[2])
-				builder.setQuantity(values[3].toDouble())
-				builder.addOrderLine()
+OrderBuilder importFile(builder, file){
+	builder.clear()
+	def lineCounter = 0
+	file.eachLine {
+		if (lineCounter != 0) {
+			def values =  it.split(';')
+			assert values.size() == 4
+			if (!builder.isInitialized()){
+				builder.setOrderNumber(values[0])
+				builder.init()
 			}
-			lineCounter++
+			builder.setLineNumber(values[1].toInteger())
+			builder.setArticleNumber(values[2])
+			builder.setQuantity(values[3].toDouble())
+			builder.addOrderLine()
 		}
-		builder.buildOrder()
-		return builder
+		lineCounter++
 	}
+	builder.buildOrder()
+	return builder
 }
